ba170461b3dfa611c4450b0d65b8e5a45259bae8
Dilmer Valecillos <dilmervst@gmail.com>
2020-07-05 16:32:17 -0600

    Added first demo demonstrating a basic path finding

---
+++ Assets/Scripts/PlayerAgent.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+
+public class PlayerAgent : Agent
+{
+    #region Exposed Instance Variables
+
+    [SerializeField]
+    private float speed = 10.0f;
+
+    [SerializeField]
+    private GameObject target;
+
+    [SerializeField]
+    private float distanceRequired = 1.5f;
+
+    [SerializeField]
+    private MeshRenderer groundMeshRenderer;
+
+    [SerializeField]
+    private Material successMaterial;
+
+    [SerializeField]
+    private Material failureMaterial;
+
+    [SerializeField]
+    private Material defaultMaterial;
+
+    #endregion
+
+    #region Private Instance Variables
+
+    private Rigidbody playerRigidbody;
+
+    private Vector3 originalPosition;
+
+    private Vector3 originalTargetPosition;
+
+    #endregion
+
+    public override void Initialize()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        originalPosition = transform.localPosition;
+        originalTargetPosition = target.transform.localPosition;
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        transform.LookAt(target.transform);
+        target.transform.localPosition = originalTargetPosition;
+        transform.localPosition = originalPosition;
+        transform.localPosition = new Vector3(Random.Range(-4, 4), originalPosition.y, originalPosition.z);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // 3 observations - x, y, z
+        sensor.AddObservation(transform.localPosition);
+
+        // 3 observations - x, y, z
+        sensor.AddObservation(target.transform.localPosition);
+
+        // 1 observation
+        sensor.AddObservation(playerRigidbody.velocity.x);
+
+        // 1 observation
+        sensor.AddObservation(playerRigidbody.velocity.z);
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        var vectorForce = new Vector3();
+
+        vectorForce.x = vectorAction[0];
+        vectorForce.z = vectorAction[1];
+
+        playerRigidbody.AddForce(vectorForce * speed);
+
+        var distanceFromTarget = Vector3.Distance(transform.localPosition, target.transform.localPosition);
+
+
+        // we are doing good
+        if (distanceFromTarget < distanceRequired)
+        {
+            AddReward(1);
+            EndEpisode();
+
+            StartCoroutine(SwapGroundMaterial(successMaterial, 0.5f));
+        }
+
+        // we are not doing so good
+        if (transform.localPosition.y < 0)
+        {
+            EndEpisode();
+
+            //SetReward(-0.1f);
+
+            // go back and punish the agent for their performance
+            StartCoroutine(SwapGroundMaterial(failureMaterial, 0.5f));
+        }
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetAxis("Horizontal"); // x
+        actionsOut[1] = Input.GetAxis("Vertical"); // z
+    }
+
+
+    private IEnumerator SwapGroundMaterial(Material mat, float time)
+    {
+        groundMeshRenderer.material = mat;
+        yield return new WaitForSeconds(time);
+        groundMeshRenderer.material = defaultMaterial;
+    }
+}
