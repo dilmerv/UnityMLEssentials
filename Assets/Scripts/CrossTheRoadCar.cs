9d7b919b4431a0b2deb57c9fb43b3283a2b7f58e
Dilmer Valecillos <dilmervst@gmail.com>
2020-07-13 17:51:56 -0600

    Starting to work on cross the road type ai

---
+++ Assets/Scripts/CrossTheRoadCar.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class CrossTheRoadCar : MonoBehaviour
+{
+    [SerializeField]
+    private AgentAvoidance agent = null;
+
+    [SerializeField]
+    private CarDirection carDirection = CarDirection.North;
+
+    [SerializeField]
+    [Range(0.5f, 25.0f)]
+    private float minSpeed = 5.0f;
+
+    [SerializeField]
+    [Range(5.0f, 150.0f)]
+    private float maxSpeed = 150.0f;
+
+    private float speed = 0;
+
+    [SerializeField]
+    private float maxDistance = -3.5f;
+
+    private Vector3 originalPosition;
+
+    public enum CarDirection
+    { 
+        South,
+        North
+    }
+
+    void Awake()
+    {
+        originalPosition = transform.localPosition;
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    void Update()
+    {
+        // if we are beyond the max distance restart the position
+        if (transform.localPosition.x <= maxDistance && carDirection == CarDirection.North || 
+            transform.localPosition.x >= maxDistance && carDirection == CarDirection.South)
+        {
+            transform.localPosition = originalPosition;
+        }
+        else
+        {
+            // move towards max distance
+            transform.localPosition = new Vector3(GetNewCarXPosition(), transform.localPosition.y,
+                transform.localPosition.z);
+        }
+    }
+
+    private float GetNewCarXPosition()
+    {
+        return carDirection == CarDirection.North ? transform.localPosition.x - (Time.deltaTime * speed) :
+            transform.localPosition.x + (Time.deltaTime * speed);
+    }
+
+    public void ResetTarget()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+        transform.localPosition = originalPosition;
+        transform.localRotation = Quaternion.identity;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag.ToLower() == "player")
+        {
+            Debug.Log("Points taken away car ran over agent");
+            agent.TakeAwayPoints();
+        }
+    }
+}
---
+++ Assets/Scripts/CrossTheRoadGoal.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CrossTheRoadGoal : MonoBehaviour
+{
+    [SerializeField]
+    private CrossTheRoadAgent agent = null;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.transform.tag.ToLower() == "player")
+        {
+            Debug.Log("Points earned as road was crossed");
+            agent.GivePoints();
+        }
+    }
+}
