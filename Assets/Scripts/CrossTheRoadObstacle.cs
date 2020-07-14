85c4e96d1b2c8ec99661280bae33543b0141453b
Dilmer Valecillos <dilmervst@gmail.com>
2020-07-14 17:35:38 -0600

    Added new 3d models and obstacles

---
+++ Assets/Scripts/CrossTheRoadObstacle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CrossTheRoadObstacle : MonoBehaviour
+{
+    public enum ObstacleType
+    { 
+        Wall,
+        Tree
+    }
+
+    [SerializeField]
+    private ObstacleType obstacleType = ObstacleType.Wall;
+
+    private CrossTheRoadAgent agent = null;
+
+    void Awake()
+    {
+        // cache agent
+        agent = transform.parent.parent.GetComponentInChildren<CrossTheRoadAgent>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.transform.tag.ToLower() == "player")
+        {
+            Debug.Log($"Collision with {obstacleType}");
+            agent.TakeAwayPoints();
+        }
+    }
+}
